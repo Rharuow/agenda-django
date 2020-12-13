64c915d256905fdc89388f180d3755f9011f5466
Harysson Soares Rodrigues de Queiroz <haryssonsoares@gmail.com>
2020-12-13 07:49:27 -0300

    debug false

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_p3r+d!%j+fl!af680_)q^$#pf7#gs8m3)g*xtg3n3)!zl&oi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost', 
